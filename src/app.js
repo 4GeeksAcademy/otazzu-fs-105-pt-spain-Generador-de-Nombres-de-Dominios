a8c0cc199146f820d4cc58812297d47ac3851d54
otazzu <srotazu@gmail.com>
2025-04-05 21:05:13 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const pronom = ['the', 'our']
+  const adj = ['great', 'mountain']
+  const nombre = ['cali', 'knight']
+  const domian = ['.com', '.net']
+
+  // loop con for
+
+  // for(let a = 0; a < pronom.length; a++){
+  //   for(let b = 0; b < adj.length; b++){
+  //     for(let c = 0; c < nombre.length; c++){
+  //       for(let d = 0; d < domian.length; d++){
+  //         console.log(pronom[a],adj[b],nombre[c],domian[d])
+  //       }
+  //     }
+  //   }
+  // }
+
+  // loop con foreach
+
+  pronom.forEach ((pro)=>{
+    adj.forEach((adje)=>{
+      nombre.forEach((nom)=>{
+        domian.forEach((dom)=>{
+          console.log(pro,adje,nom,dom)
+        })
+      })
+    })
+  })
+}
